8e1935da46a8fc8dbe86ac9e3879408c5a55ac7c
Simon Strandgaard <neoneye@gmail.com>
2024-05-09 12:38:43 +0200

    traverse all the files inside the ARC dataset.

--- proof_of_concept/llamacpp_finetune_lab1/main.py
+++ proof_of_concept/llamacpp_finetune_lab1/main.py
@@ -1,7 +1,26 @@
 import my_arc_thing
 import my_arc_thing.arc_json_model as ajm
 import os
+import json
 
-path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "my_arc_thing", "assets", "68b67ca3.json")
-task = ajm.Task.load(path)
-print(task)
+def process_json_file(file_path):
+    """
+    Load the ARC task.
+    Check if it gets solved correctly by the LLM.
+    """
+    print(f"Processing: {file_path}")
+    task = ajm.Task.load(file_path)
+    print(task)
+
+def main():
+    root_dir = '/Users/neoneye/git/arc-dataset-collection/dataset/ARC/data'
+
+    for subdir, dirs, files in os.walk(root_dir):
+        files = sorted(files)
+        for file in files:
+            if file.endswith('.json'):
+                file_path = os.path.join(subdir, file)
+                process_json_file(file_path)
+
+if __name__ == "__main__":
+    main()

659bb59bb8b9ce13505e085d06e290f9b9a44559
Simon Strandgaard <neoneye@gmail.com>
2024-05-17 12:46:11 +0200

    do one iteration with the random grid

--- proof_of_concept/gameoflife_finetune_lab1/main.py
+++ proof_of_concept/gameoflife_finetune_lab1/main.py
@@ -1,10 +1,8 @@
 from game_of_life import game_of_life
+from random_game_of_life import generate_random_game_of_life_string
 
-# Example usage
-input_str = "..*,.*.,*.."
-output_str = game_of_life(input_str)
-print(output_str)
-
-
-#if __name__ == "__main__":
-#    main()
+input = generate_random_game_of_life_string(seed=43)
+output = game_of_life(input)
+print(input)
+print('---')
+print(output)

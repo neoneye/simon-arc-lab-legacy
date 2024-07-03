d06651218134edc83f7a667f9f4e549d07754b6d
Simon Strandgaard <neoneye@gmail.com>
2024-07-03 20:49:03 +0200

    testing the RLE segment decoding

--- proof_of_concept/rle_compression/test_deserialize.py
+++ proof_of_concept/rle_compression/test_deserialize.py
@@ -1,15 +1,27 @@
 import unittest
 import numpy as np
-from deserialize import deserialize
+from deserialize import deserialize, decode_rle_row
 
 class TestDeserialize(unittest.TestCase):
+    def test_decode_rle_row_0(self):
+        a = "0"
+        actual = decode_rle_row(a, 11)
+        expected = [0] * 11
+        self.assertEqual(actual, expected)
+
+    def test_decode_rle_row_1(self):
+        a = "02a12e0"
+        actual = decode_rle_row(a, 11)
+        expected = [0, 2, 1, 1, 2, 0, 0, 0, 0, 0, 0]
+        self.assertEqual(actual, expected)
+
     def test_deserialize_full(self):
-        a = "11 11 0,0c2e0,02a12e0,,,,,,0c2e0,0,"
+        a = "11 12 0,0c2e0,02a12e0,,,,,,0c2e0,0,,"
         actual = deserialize(a)
         # print(actual)
 
         # Create a 11x11 array filled with zeros
-        expected = np.zeros((11, 11), dtype=np.uint8)
+        expected = np.zeros((12, 11), dtype=np.uint8)
 
         # Fill in the specific values
         expected[1:9, 1:5] = [

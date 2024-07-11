496a17ae752b7726e8ad508d0095121012cbbcfa
Simon Strandgaard <neoneye@gmail.com>
2024-07-11 13:29:03 +0200

    bresenham_line algorithm

--- proof_of_concept/rle_compression/test_image_util.py
+++ proof_of_concept/rle_compression/test_image_util.py
@@ -184,5 +184,39 @@ class TestImageUtil(unittest.TestCase):
         actual = image_rotate_180(input)
         self.assertTrue(np.array_equal(actual, expected))
 
+    def test_bresenham_line_diagonal(self):
+        image = np.zeros((3, 3), dtype=np.uint8)
+        actual = bresenham_line(image, 0, 0, 2, 2, 1)
+
+        expected = np.zeros((3, 3), dtype=np.uint8)
+        expected[0, 0] = 1
+        expected[1, 1] = 1
+        expected[2, 2] = 1
+
+        self.assertTrue(np.array_equal(actual, expected))
+
+    def test_bresenham_line_horizontal(self):
+        image = np.zeros((3, 4), dtype=np.uint8)
+        actual = bresenham_line(image, 0, 0, 3, 0, 1)
+
+        expected = np.zeros((3, 4), dtype=np.uint8)
+        expected[0, 0] = 1
+        expected[0, 1] = 1
+        expected[0, 2] = 1
+        expected[0, 3] = 1
+
+        self.assertTrue(np.array_equal(actual, expected))
+
+    def test_bresenham_line_horizontal(self):
+        image = np.zeros((3, 4), dtype=np.uint8)
+        actual = bresenham_line(image, 1, 0, 1, 2, 1)
+
+        expected = np.zeros((3, 4), dtype=np.uint8)
+        expected[0, 1] = 1
+        expected[1, 1] = 1
+        expected[2, 1] = 1
+
+        self.assertTrue(np.array_equal(actual, expected))
+
 if __name__ == '__main__':
     unittest.main()

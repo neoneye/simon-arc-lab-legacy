15c6b985ddc268c8a9589e599c5d586c593b3dfd
Simon Strandgaard <neoneye@gmail.com>
2024-11-22 21:27:22 +0100

    generated dataset

--- simon_arc_dataset_run/dataset_solve_skew.py
+++ simon_arc_dataset_run/dataset_solve_skew.py
@@ -34,7 +34,7 @@ def generate_task_skew(seed: int, direction: SkewDirection) -> Task:
     task = Task()
     task.metadata_task_id = f'skew_{direction.name.lower()}'
     min_image_size = 1
-    max_image_size = 5
+    max_image_size = 12
 
     colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     random.Random(seed + 3).shuffle(colors)
@@ -88,7 +88,7 @@ def generate_task_unskew(seed: int, direction: SkewDirection) -> Task:
     task = Task()
     task.metadata_task_id = f'unskew_{direction.name.lower()}'
     min_image_size = 1
-    max_image_size = 5
+    max_image_size = 12
 
     colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     random.Random(seed + 3).shuffle(colors)
@@ -165,7 +165,7 @@ generator = DatasetGenerator(
     generate_dataset_item_list_fn=generate_dataset_item_list
 )
 generator.generate(
-    seed=1222123425,
+    seed=1223123425,
     max_num_samples=1000,
     max_byte_size=1024*1024*100
 )

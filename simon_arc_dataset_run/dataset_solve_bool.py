c985727c10e18b7b62507af17c2cbae152c8ccfd
Simon Strandgaard <neoneye@gmail.com>
2024-11-22 14:45:45 +0100

    generated dataset

--- simon_arc_dataset_run/dataset_solve_bool.py
+++ simon_arc_dataset_run/dataset_solve_bool.py
@@ -53,7 +53,7 @@ def generate_task_bool_transformation(seed: int, transformation_id: str) -> Task
     task = Task()
     task.metadata_task_id = transformation_id
     min_image_size = 2
-    max_image_size = 7
+    max_image_size = 9
 
     input_colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     random.Random(seed + 3).shuffle(input_colors)
@@ -174,7 +174,8 @@ def generate_task_bool_transformation(seed: int, transformation_id: str) -> Task
 
 def generate_dataset_item_list_inner(seed: int, task: Task, transformation_id: str) -> list[dict]:
     builder = DatasetItemListBuilder(seed, task, DATASET_NAMES, BENCHMARK_DATASET_NAME, transformation_id)
-    builder.append_image_randomized()
+    # builder.append_image_randomized()
+    builder.append_image_rawpixel_output()
     return builder.dataset_items()
 
 def generate_dataset_item_list(seed: int) -> list[dict]:
@@ -195,7 +196,7 @@ generator = DatasetGenerator(
     generate_dataset_item_list_fn=generate_dataset_item_list
 )
 generator.generate(
-    seed=241000771,
+    seed=242100771,
     max_num_samples=1000,
     max_byte_size=1024*1024*100
 )

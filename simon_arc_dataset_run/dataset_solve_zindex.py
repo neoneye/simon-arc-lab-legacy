c8fb5134a60ab894b66572154109339f97c8944f
Simon Strandgaard <neoneye@gmail.com>
2024-11-23 00:05:43 +0100

    generated dataset

--- simon_arc_dataset_run/dataset_solve_zindex.py
+++ simon_arc_dataset_run/dataset_solve_zindex.py
@@ -47,7 +47,7 @@ def generate_task_mask_of_primary_rectangle(seed: int) -> Task:
     # count_test = 1
     task = Task()
     min_image_size = 6
-    max_image_size = 20
+    max_image_size = 15
 
     # input colors
     colors_input = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -118,7 +118,7 @@ def generate_task_mask_of_obscured_rectangle(seed: int) -> Task:
     # count_test = 1
     task = Task()
     min_image_size = 6
-    max_image_size = 18
+    max_image_size = 15
 
     # input colors
     colors_input = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -233,7 +233,7 @@ def generate_task_mask_of_intersection_rectangle(seed: int) -> Task:
     # count_test = 1
     task = Task()
     min_image_size = 6
-    max_image_size = 16
+    max_image_size = 15
 
     # input colors
     colors_input = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -449,7 +449,8 @@ def generate_task_move_obscured_rectangle_to_top(seed: int) -> Task:
 
 def generate_dataset_item_list_inner(seed: int, task: Task, transformation_id: str) -> list[dict]:
     builder = DatasetItemListBuilder(seed, task, DATASET_NAMES, BENCHMARK_DATASET_NAME, transformation_id)
-    builder.append_image_randomized()
+    # builder.append_image_randomized()
+    builder.append_image_rawpixel_output()
     return builder.dataset_items()
 
 def generate_dataset_item_list(seed: int) -> list[dict]:
@@ -474,7 +475,7 @@ generator = DatasetGenerator(
     generate_dataset_item_list_fn=generate_dataset_item_list
 )
 generator.generate(
-    seed=218200911,
+    seed=218300911,
     max_num_samples=1000,
     max_byte_size=1024*1024*100
 )

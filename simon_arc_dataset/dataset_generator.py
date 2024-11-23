99a30b8a542a0ab379fe6f63c42e7b2988574cfb
Simon Strandgaard <neoneye@gmail.com>
2024-11-23 15:19:16 +0100

    Made a new base class for dataset generators, that doesn't use a function callback.

--- simon_arc_dataset/dataset_generator.py
+++ simon_arc_dataset/dataset_generator.py
@@ -55,3 +55,54 @@ class DatasetGenerator:
     def inspect(self):
         plot_prompt_length_distribution(self.dataset_items)
         plot_response_length_distribution(self.dataset_items)
+
+class DatasetGenerator2:
+    def __init__(self):
+        self.row_strings = None
+        self.dataset_items = None
+
+    def generate_dataset_item_list(self, seed: int, show: bool) -> list[dict]:
+        raise NotImplementedError("This method should be overridden by subclasses.")
+
+    def generate(self, seed: int, max_num_samples=1000, max_byte_size=10*1024*1024, show: bool = False):
+        row_strings = []
+        dataset_items = []
+        file_size = 0
+        stop = False
+        with tqdm(total=max_num_samples, desc="Generating dataset", ncols=100, unit="sample", mininterval=1.0, dynamic_ncols=True, leave=True) as pbar:
+            for i in range(max_num_samples):
+                if stop:
+                    break
+                items = self.generate_dataset_item_list(seed + i + 1000, show)
+                for item in items:
+                    row_string = json.dumps(item, separators=(',', ':')) + '\n'
+                    bytes = len(row_string)
+                    if file_size + bytes > max_byte_size:
+                        stop = True
+                        break
+                    if len(row_strings) >= max_num_samples:
+                        stop = True
+                        break
+                    file_size += bytes
+                    row_strings.append(row_string)
+                    dataset_items.append(item)
+                    pbar.update(1)
+        random.Random(seed).shuffle(row_strings)
+
+        self.row_strings = row_strings
+        self.dataset_items = dataset_items
+
+    def save(self, file_path: str):
+        """
+        Save the generated dataset to a "jsonl" file
+        """
+        row_strings = self.row_strings
+        with open(file_path, 'w') as f:
+            for row_string in row_strings:
+                f.write(row_string)
+        file_size = os.path.getsize(file_path)
+        print(f"Generated {len(row_strings)} samples, saved to {file_path}, file size: {file_size} bytes.")
+
+    def inspect(self):
+        plot_prompt_length_distribution(self.dataset_items)
+        plot_response_length_distribution(self.dataset_items)
